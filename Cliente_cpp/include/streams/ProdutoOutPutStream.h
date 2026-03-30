f00c0edc8ea2caf750f5865cba064205b3217631
F-K1 <fksilva@alu.ufc.br>
2026-03-30 11:41:39 -0300

    Inclusão da Subclasse Celular + Criação da OutputStream + ajustes no projeto

---
+++ Cliente_cpp/include/streams/ProdutoOutPutStream.h
@@ -0,0 +1,20 @@
+#ifndef PRODUTO_OUTPUT_STREAM_H
+#define PRODUTO_OUTPUT_STREAM_H
+
+#include <vector>
+#include <ostream>
+#include "Produto.h"
+
+class ProdutoOutputStream {
+private:
+    std::vector<Produto> produtos;
+    int qtd;
+    std::ostream& out;
+
+public:
+    ProdutoOutputStream(const std::vector<Produto>& produtos, int qtd, std::ostream& out);
+
+    void write();
+};
+
+#endif
