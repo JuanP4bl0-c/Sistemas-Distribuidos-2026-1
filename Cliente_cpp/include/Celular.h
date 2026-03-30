4eda869562bc7585e3092be4b4ae0133034b1048
F-K1 <fksilva@alu.ufc.br>
2026-03-30 00:38:16 -0300

    Limpeza de arquivos compilados + ajuste do projeto

---
+++ Cliente_cpp/include/Celular.h
@@ -0,0 +1,16 @@
+#include <string>
+#include "Produto.h"
+
+class Celular : public Produto {
+private:
+    std::string marca;
+    std::string modelo;
+public:
+    Celular(int id, std::string nome, std::string descricao, float preco, int quantidade_estoque, std::string marca, std::string modelo);
+
+    std::string getMarca() const;
+    std::string getModelo() const;
+    void setMarca(const std::string& marca);
+    void setModelo(const std::string& modelo);
+    void mostrar() const override;
+};
