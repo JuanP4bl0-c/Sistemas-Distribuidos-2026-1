db0bbe95d184cbe88c2dcbfad12ae132386d89f7
F-K1 <fksilva@alu.ufc.br>
2026-04-15 22:57:32 -0300

    Limpeza de conflitos

--- Cliente_cpp/include/streams/ProdutoOutPutStream.h
+++ Cliente_cpp/include/streams/ProdutoOutPutStream.h
@@ -2,19 +2,6 @@
 #define PRODUTO_OUTPUT_STREAM_H
 
 #include <vector>
-<<<<<<< HEAD
-#include <ostream>
-#include "modelos/Produto.h"
-
-class ProdutoOutputStream {
-private:
-    std::vector<Produto> produtos;
-    int qtd;
-    std::ostream& out;
-
-public:
-    ProdutoOutputStream(const std::vector<Produto>& produtos, int qtd, std::ostream& out);
-=======
 #include <memory>
 #include <ostream>
 #include <string>
@@ -41,13 +28,8 @@ public:
         const std::vector<std::shared_ptr<Produto>>& produtos,
         int qtd,
         std::ostream& out);
->>>>>>> 3a786a2 (Cliente_cpp atualizada da branch client_cpp)
 
     void write();
 };
 
-<<<<<<< HEAD
-#endif
-=======
 #endif // PRODUTO_OUTPUT_STREAM_H
->>>>>>> 3a786a2 (Cliente_cpp atualizada da branch client_cpp)
